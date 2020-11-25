c91118f7d954319dd901e1745f0211ef0b6cf708
edib <edibersh@gmail.com>
2020-11-25 11:51:22 +0200

    Initial commit

---
+++ app/src/main/java/com/e/mysensordata2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.e.mysensordata2
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mSensorManager: SensorManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mSensorManager =
+            getSystemService(Context.SENSOR_SERVICE) as SensorManager?
+
+        val sensorList: List<Sensor> = mSensorManager!!.getSensorList(Sensor.TYPE_ALL)
+
+        val sensorText = StringBuilder()
+
+        for (currentSensor in sensorList) {
+            sensorText.append(currentSensor.name).append(
+                System.getProperty("line.separator"))
+        }
+
+        textView.text = sensorText
+    }
+}
